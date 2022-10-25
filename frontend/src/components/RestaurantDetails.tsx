e116f3f44bb874ddb091055982c4380718a19ee9
Gentrit Uka <gentrit-uka@live.com>
2022-10-25 11:35:11 +0200

    updated

---
+++ frontend/src/components/RestaurantDetails.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function RestaurantDetails() {
+  return (
+    <div>
+        <image />
+        <main>
+            <nav>
+                <button>Overview</button>
+                <button>Photos</button>
+                <button>Menu</button>
+                <button>Reviews</button>
+            </nav>
+            <section></section>
+        </main>
+    </div>
+  )
+}
+
+export default RestaurantDetails
