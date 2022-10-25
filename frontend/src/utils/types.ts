e9ba5094d0ae62b668be18c4bdde6955d4e32d05
Gentrit Uka <gentrit-uka@live.com>
2022-10-25 19:02:08 +0200

    updated

--- frontend/src/utils/types.ts
+++ frontend/src/utils/types.ts
@@ -1,3 +1,14 @@
+export type User = {
+    id: string;
+    name: string;
+    email: string;
+    password: string;
+    role: string;
+    reviews: Review[];
+    reservations: Reservation[];
+    restaurants: Restaurant[];
+}
+
 export type Restaurant = {
     id: number
     name: string    
@@ -9,4 +20,41 @@ export type Restaurant = {
     cuisineInfo: string
     priceInfo: string
     workHours: string
+    diningStyle: string
+    dressCode: string
+    additional: string
+    reviews: Review[]
+    images: Image[]
+    menus: Menu[]
+    reservations: Reservation[]
+}
+
+export type Image = {
+    id: number
+    url: string
+    restaurantId: number
+}
+
+export type Review = {
+    id: number
+    review: string
+    rating: number
+    restaurantId: number
+    userId: number
+}
+
+export type Menu = {
+    id: number
+    name: string
+    price: number
+    restaurantId: number
+}
+
+export type Reservation = {
+    id: number
+    date: string
+    time: string
+    restaurantId: number
+    userId: number
+    claimedBy: User
 }
