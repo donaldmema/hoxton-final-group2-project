41db672df15277003580037cdc605eb272af6864
Gentrit Uka <gentrit-uka@live.com>
2022-10-25 19:29:24 +0200

    updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -77,8 +77,8 @@ function App() {
         <Route index element={<Navigate to="/homepage" />} />
         <Route path="/homepage" element={<HomePage />} />
         <Route path="/logedin" element={<LogedinPage />} />
-        <Route path="/restaurant" element={<RestaurantDetails restaurants={restaurants}/>} />
-        <Route path="/restaurant/:id" element={<RestaurantDetails restaurants={restaurants} />} />
+        <Route path="/restaurant" element={<RestaurantDetails/>} />
+        <Route path="/restaurant/:id" element={<RestaurantDetails />} />
       </Routes>
     </div>
   );
