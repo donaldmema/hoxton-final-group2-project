099814be87059f4bc7bfde03f7b5b14ec5d753f7
Gentrit Uka <gentrit-uka@live.com>
2022-10-25 16:54:46 +0200

    updated

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -1,7 +1,1 @@
-export function getDateToday(date: string){
-    const today = new Date(date)
-    const day = today.getDay()
-    const month = today.getMonth()
-    const year = today.getFullYear()
-    return `${day}/${month}/${year}`
-}
+
