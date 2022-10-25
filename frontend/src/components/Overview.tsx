3b1690d58c36303ee19a461f6a9ffe3d8c4e006e
Gentrit Uka <gentrit-uka@live.com>
2022-10-25 11:50:57 +0200

    updated

--- frontend/src/components/Overview.tsx
+++ frontend/src/components/Overview.tsx
@@ -3,9 +3,12 @@ import React from 'react'
 function Overview() {
   return (
     <div>
-        <h1>
-            
-        </h1>
+        <h1></h1>
+        <div>
+            <div>
+                <MdOutlineModeComment />
+            </div>
+        </div>
     </div>
   )
 }
