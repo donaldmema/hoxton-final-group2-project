a2792a6ec88be90e34d720faa820ec93f012a1e9
Gentrit Uka <gentrit-uka@live.com>
2022-10-25 11:21:18 +0200

    updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { Routes } from 'react-router-dom'
 
 function App() {
 
   return (
     <div className="App">
-      
+      <Routes>
+        
+      </Routes>
     </div>
   )
 }
