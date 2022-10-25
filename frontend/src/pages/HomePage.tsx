6d36d849de24d9c3a64214c5e3731db391e5e5f9
edonajemini <74864689+edonajemini@users.noreply.github.com>
2022-10-25 11:50:45 +0200

    Updates

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from '../components/Header'
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <div>
+         <Header />
+    </div>
   )
 }
 
