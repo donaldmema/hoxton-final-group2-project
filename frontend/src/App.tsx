9271e83904b89dc1ff34fcd882f52f48ed79ba67
Gentrit Uka <gentrit-uka@live.com>
2022-10-25 18:41:26 +0200

    pushed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,14 +4,79 @@ import "./App.css";
 import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import RestaurantDetails from "./pages/RestaurantDetails";
+import { Restaurant } from "./utils/types";
 
 function App() {
+  const [restaurants, setRestaurants] = useState([
+    {
+      id: 1,
+      name: "Burger King",
+      address: "1234 Main St",
+      city: "New York",
+      description: "Burgers and fries",
+      profileImg: "https://picsum.photos/200",
+      rating: 4.5,
+      cuisineInfo: "American",
+      priceInfo: "$",
+      workHours: "9am - 9pm",
+    },
+    {
+      id: 2,
+      name: "Burger King",
+      address: "1234 Main St",
+      city: "New York",
+      description: "Burgers and fries",
+      profileImg: "https://picsum.photos/200",
+      rating: 4.5,
+      cuisineInfo: "American",
+      priceInfo: "$",
+      workHours: "9am - 9pm",
+    },
+    {
+      id: 2,
+      name: "Burger King",
+      address: "1234 Main St",
+      city: "New York",
+      description: "Burgers and fries",
+      profileImg: "https://picsum.photos/200",
+      rating: 4.5,
+      cuisineInfo: "American",
+      priceInfo: "$",
+      workHours: "9am - 9pm",
+    },
+    {
+      id: 2,
+      name: "Burger King",
+      address: "1234 Main St",
+      city: "New York",
+      description: "Burgers and fries",
+      profileImg: "https://picsum.photos/200",
+      rating: 4.5,
+      cuisineInfo: "American",
+      priceInfo: "$",
+      workHours: "9am - 9pm",
+    },
+    {
+      id: 2,
+      name: "Burger King",
+      address: "1234 Main St",
+      city: "New York",
+      description: "Burgers and fries",
+      profileImg: "https://picsum.photos/200",
+      rating: 4.5,
+      cuisineInfo: "American",
+      priceInfo: "$",
+      workHours: "9am - 9pm",
+    },
+  ]);
+
+
   return (
     <div className="App">
       <Routes>
         <Route index element={<Navigate to="/homepage" />} />
         <Route path="/homepage" element={<HomePage />} />
-        <Route path="/restaurant" element={<RestaurantDetails />} />
+        <Route path="/restaurant" element={<RestaurantDetails restaurants={restaurants}/>} />
       </Routes>
     </div>
   );
