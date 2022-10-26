e43c9d5b8c2b46c6b2db85dfe94d2f4617b721e7
Elona Kajtazi <MY_NAME@example.com>
2022-10-26 12:32:49 +0300

    ....

--- frontend/src/components/Restaurants.tsx
+++ frontend/src/components/Restaurants.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 import stars from "../assets/stars.png";
 import { Restaurant } from "../utils/types";
@@ -26,27 +27,29 @@ export function Restaurants() {
         {restaurants.map((restaurant) => (
           <>
             {/* <Link to={`/restaurant-detail/${restaurants.id}`}> */}
-            <div className="restaurant-feed-item">
-              <img src={restaurant.profileImg} width="250px" height="135px" />
-              <div className="restaurant-content">
-                <h3>{restaurant.name}</h3>
-                <div className="rating">
-                  <img src={stars} width="100px" height="20px" />{" "}
-                  <h5>{restaurant.reviews.length} reviews</h5>
+            <Link to={`/restaurants/${restaurant.id}`}>
+              <div className="restaurant-feed-item">
+                <img src={restaurant.profileImg} width="250px" height="135px" />
+                <div className="restaurant-content">
+                  <h3>{restaurant.name}</h3>
+                  <div className="rating">
+                    <img src={stars} width="100px" height="20px" />{" "}
+                    <h5>{restaurant.reviews.length} reviews</h5>
+                  </div>
+                  <p>
+                    {restaurant.cuisineInfo} • {restaurant.priceInfo} •{" "}
+                    {restaurant.city}
+                  </p>
+                  <button
+                    onClick={() => {
+                      setSeeAvailableDates(true);
+                    }}
+                  >
+                    Find next availble
+                  </button>
                 </div>
-                <p>
-                  {restaurant.cuisineInfo} • {restaurant.priceInfo} •{" "}
-                  {restaurant.city}
-                </p>
-                <button
-                  onClick={() => {
-                    setSeeAvailableDates(true);
-                  }}
-                >
-                  Find next availble
-                </button>
               </div>
-            </div>
+            </Link>
             {seeAvailableDates ? (
               <FindNextAvailable setSeeAvailableDates={setSeeAvailableDates} />
             ) : null}
