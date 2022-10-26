164e84ca94aeffd4d3d7368de5473438d73aaba8
edonajemini <74864689+edonajemini@users.noreply.github.com>
2022-10-26 02:24:56 +0200

    Finished the sign up page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,8 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import RestaurantDetails from "./pages/RestaurantDetails";
 import LogedinPage from "./pages/LogedinPage";
+import { SignInForm } from "./pages/SignInForm";
+import { SignUpForm } from "./pages/SignUpForm";
 
 function App() {
   
@@ -14,6 +16,8 @@ function App() {
         <Route index element={<Navigate to="/homepage" />} />
         <Route path="/homepage" element={<HomePage />} />
         <Route path="/logedin" element={<LogedinPage />} />
+        <Route path="/signin" element={<SignInForm />} />
+        <Route path="/signup" element={<SignUpForm />} />
         <Route path="/restaurants" element={<RestaurantDetails/>} />
         <Route path="/restaurants/:id" element={<RestaurantDetails />} />
       </Routes>
---
+++ frontend/src/pages/SignInForm.tsx
@@ -0,0 +1,12 @@
+import { Bussines } from "../components/Bussines";
+import Header from "../components/Header";
+import "./SignInForm.css"
+
+export function SignInForm(){
+    return(
+        <>
+        <Bussines />
+        <Header />
+        </>
+    )
+}
