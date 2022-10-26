42c124bcf24e687fbe4ca547102c5f56a70cc35d
donaldmema <mema.donald@gmail.com>
2022-10-26 12:25:17 +0200

    Add login functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,57 @@
 import "./App.css";
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
+import * as API from "./api";
 import HomePage from "./pages/HomePage";
 import RestaurantDetails from "./pages/RestaurantDetails";
 import LogedinPage from "./pages/LogedinPage";
 import { SignInForm } from "./pages/SignInForm";
 import { SignUpForm } from "./pages/SignUpForm";
 import { PageNotFound } from "./pages/PageNotFound";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const navigate = useNavigate();
+
+  function signIn(data: { user: any; token: string }) {
+    setCurrentUser(data.user);
+    localStorage.token = data.token;
+  }
+
+  function signOut() {
+    setCurrentUser(null);
+    navigate("/homepage");
+    localStorage.removeItem("token");
+  }
+
+  function getDate(sqlDate: any) {
+    let dateString = sqlDate.toString();
+
+    return dateString;
+  }
+
+  useEffect(() => {
+    if (localStorage.token) {
+      API.validate().then((data) => {
+        if (data.error) {
+          alert(data.error);
+          localStorage.removeItem("token");
+        } else {
+          signIn(data);
+          navigate("/select-community");
+        }
+      });
+    }
+  }, []);
+
   return (
     <div className="App">
       <Routes>
         <Route index element={<Navigate to="/homepage" />} />
         <Route path="/homepage" element={<HomePage />} />
         <Route path="/logedin" element={<LogedinPage />} />
-        <Route path="/signin" element={<SignInForm />} />
-        <Route path="/signup" element={<SignUpForm />} />
+        <Route path="/signin" element={<SignInForm signIn={signIn} />} />
+        <Route path="/signup" element={<SignUpForm signIn={signIn} />} />
         <Route path="/restaurants" element={<RestaurantDetails />} />
         <Route path="/restaurants/:id" element={<RestaurantDetails />} />
         <Route path="*" element={<PageNotFound />} />
--- frontend/src/pages/SignUpForm.tsx
+++ frontend/src/pages/SignUpForm.tsx
@@ -1,64 +1,96 @@
-import JoinUs from "../components/ JoinUs"
-import { Bussines } from "../components/Bussines"
-import Header from "../components/Header"
-import "./SignUpForm.css"
-export function SignUpForm(){
-    return(
-        <>
-        <Bussines />
-        <Header />
-        <div className="signup-page">
-        <JoinUs/>
+import { useNavigate } from "react-router-dom";
+import * as API from "../api";
+import JoinUs from "../components/ JoinUs";
+import { Bussines } from "../components/Bussines";
+import Header from "../components/Header";
+import "./SignUpForm.css";
+
+type Props = {
+  signIn: (data: { user: any; token: string }) => void;
+};
+
+export function SignUpForm({ signIn }: Props) {
+  const navigate = useNavigate();
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const name = form.fullName.value;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    if (email && password && name) {
+      API.signup({ email, password, name }).then((data) => {
+        if (data.error) {
+          alert(data.error);
+        } else {
+          //sign them in
+          signIn(data);
+          navigate("/select-community");
+        }
+      });
+    }
+  }
+
+  return (
+    <>
+      <Bussines />
+      <Header />
+      <div className="signup-page">
+        <JoinUs />
         <div className="sign-up-form-container">
-        <h2>Create your account</h2>
-        <h4>
-          Signing up as <span> </span>
-          <a href="/signin"> (not you?)</a>
-        </h4>
-        <form
-          className="sign-up-form-section"
-        >
-          <label htmlFor="name">Full Name</label>
-          <input
-            type="text"
-            placeholder="Full Name"
-            name="fullName"
-            id="name"
-            required
-          />
-           <label htmlFor="name">Email</label>
-          <input
-            type="text"
-            placeholder="Email"
-            name="email"
-            id="email"
-            required
-          />
-          <label htmlFor="password">
-            Password <span>*</span> <br />
-            <p>Use at least 8 characters</p>
-          </label>
+          <h2>Create your account</h2>
+          <h4>
+            Signing up as <span> </span>
+            <a href="/signin"> (not you?)</a>
+          </h4>
+          <form
+            className="sign-up-form-section"
+            onSubmit={(event) => handleSubmit(event)}
+          >
+            <label htmlFor="name">Full Name</label>
+            <input
+              type="text"
+              placeholder="Full Name"
+              name="fullName"
+              id="name"
+              required
+            />
+            <label htmlFor="name">Email</label>
+            <input
+              type="text"
+              placeholder="Email"
+              name="email"
+              id="email"
+              required
+            />
+            <label htmlFor="password">
+              Password <span>*</span> <br />
+              <p>Use at least 8 characters</p>
+            </label>
 
-          <input
-            type="password"
-            id="password"
-            placeholder="Password"
-            name="password"
-            required
-          />
+            <input
+              type="password"
+              id="password"
+              placeholder="Password"
+              name="password"
+              required
+            />
 
-          <p>
-            By creating an account or logging in, you understand and agree to
-             <a href=""> Terms</a> and <a href=""> Conditions</a>. You also acknowledge our{" "}
-            <a href="">Cookies</a> and <a href="">Privacy</a> policies.
-          </p>
+            <p>
+              By creating an account or logging in, you understand and agree to
+              <a href=""> Terms</a> and <a href=""> Conditions</a>. You also
+              acknowledge our <a href="">Cookies</a> and <a href="">Privacy</a>{" "}
+              policies.
+            </p>
 
-          <button className="sign-up-btn" type="submit">
-            Create account
-          </button>
-        </form>
+            <button className="sign-up-btn" type="submit">
+              Create account
+            </button>
+          </form>
+        </div>
       </div>
-      </div>
-        </>
-    )
+    </>
+  );
 }
