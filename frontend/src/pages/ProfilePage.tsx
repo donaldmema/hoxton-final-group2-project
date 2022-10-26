3ab9c90a10dc843fd294a2533153ef835ad0b866
edonajemini <74864689+edonajemini@users.noreply.github.com>
2022-10-26 14:00:01 +0200

    Updates

---
+++ frontend/src/pages/ProfilePage.tsx
@@ -0,0 +1,11 @@
+import { Bussines } from "../components/Bussines";
+import Header from "../components/Header";
+
+export function ProfilePage(){
+    return(
+        <>
+        <Bussines/>
+        <Header/>
+        </>
+    )
+}
